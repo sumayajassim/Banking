964afc4e1394f0dbf40267c29476f6f9f2487a00
sumayajassim <sumayajassim1@gmail.com>
2024-11-14 00:42:41 +0300

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,36 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Sumaya",
+    lastName: "Dabwan",
+    email: "sumayajassim1@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.234}
+          />
+        </header>
+        RECENT TRASACTIONS
+      </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
+    </section>
+  );
 };
 
 export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
 import type { Metadata } from "next";
-import { Inter, IBM_Plex_Serif} from "next/font/google"
-import localFont from "next/font/local";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-
-const inter = Inter({subsets: ["latin"], variable:'--font-inter'});
-const ibmPlexSerif  = IBM_Plex_Serif({
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ["latin"],
-  weight: ['400', '700'],
-  variable: '--font-ibm-plex-serif'
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
 });
 
 export const metadata: Metadata = {
   title: "Horizon",
   description: "Horizon is a modern banking platform for everyone",
-  icons:{
-    icon: 'icons/logo.svg'
-  }
+  icons: {
+    icon: "icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -26,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.variable} ${ibmPlexSerif.variable} `}
-      >
+      <body className={`${inter.variable} ${ibmPlexSerif.variable} `}>
         {children}
       </body>
     </html>
